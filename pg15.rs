68f1776804c6397c9b9b3a035799ffcfbbfc7488
Vallagi Salmon Raj <155515434+SalmonRaj125@users.noreply.github.com>
2025-07-04 11:23:49 +0530

    Update pg15.rs

--- pg15.rs
+++ pg15.rs
@@ -2,8 +2,6 @@ fn main() {
     let mut i = 1;
 
     println!("First 10 Natural Numbers:");
-
-    // Using an infinite loop with break condition
     loop {
         if i > 10 {
             break;
