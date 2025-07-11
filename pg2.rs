4f834b5cf195f8fda9980bde76e9b58cc218fd3c
Vallagi Salmon Raj <155515434+SalmonRaj125@users.noreply.github.com>
2025-07-11 11:20:25 +0530

    Update pg2.rs

--- pg2.rs
+++ pg2.rs
@@ -1,6 +1,5 @@
 fn main() {
     for i in 1..=5 {
-        // Repeat the character i times and print it
         let line = std::iter::repeat(i.to_string())
             .take(i)
             .collect::<String>();
