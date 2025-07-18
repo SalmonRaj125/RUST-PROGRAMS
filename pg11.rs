d5fea26e10c424f1c80b36f91ed5c329a0115f7e
Vallagi Salmon Raj <155515434+SalmonRaj125@users.noreply.github.com>
2025-07-18 12:05:17 +0530

    Update pg11.rs

--- pg11.rs
+++ pg11.rs
@@ -1,17 +1,12 @@
 use std::io;
 
 fn main() {
-    // Ask user to enter a number
     println!("Enter a number:");
 
-    // Read input from user
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input");
-
-    // Convert input string to a number (i32)
     let number: i32 = input.trim().parse().expect("Please enter a valid integer");
 
-    // Check if number is positive, negative or zero
     if number > 0 {
         println!("The number {} is Positive", number);
     } else if number < 0 {
